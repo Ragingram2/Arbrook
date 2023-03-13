8ac375b5695a9fa909b6a724309bf97d0a2c4e21
Rowan Ramsey <blazinram@gmail.com>
2023-03-13 20:35:34 +0100

    solved the circular dependency, we now have dx11 window rendering, and started on shaders for actually rendering in dx11

--- arbrook/rendering/data/DirectX/buffer.hpp
+++ arbrook/rendering/data/DirectX/buffer.hpp
@@ -1,8 +1,30 @@
 #pragma once
+#include <d3d11.h>
+#include <d3dx11.h>
+#include <d3dx10.h>
+
+// include the Direct3D Library file
+#pragma comment (lib, "d3d11.lib")
+#pragma comment (lib, "d3dx11.lib")
+#pragma comment (lib, "d3dx10.lib")
+
 #include "rendering/data/bufferconcepts.hpp"
 
 namespace rythe::rendering::internal
 {
+	struct VERTEX
+	{
+		float X, Y, Z;      // position
+		D3DXCOLOR Color;    // color
+	};
+
+	VERTEX OurVertices[] =
+	{
+		{0.0f, 0.5f, 0.0f, D3DXCOLOR(1.0f, 0.0f, 0.0f, 1.0f)},
+		{0.45f, -0.5, 0.0f, D3DXCOLOR(0.0f, 1.0f, 0.0f, 1.0f)},
+		{-0.45f, -0.5f, 0.0f, D3DXCOLOR(0.0f, 0.0f, 1.0f, 1.0f)}
+	};
+
 	template<typename T, typename dataType>
 		requires rendering::validType<T>
 	struct buffer
