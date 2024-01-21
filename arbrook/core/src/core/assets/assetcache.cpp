dc4eaa6a48ff61110f7a247bc4731be408cea1a6
Rowan Ramsey <blazinram@gmail.com>
2024-01-21 23:26:08 +0100

    ran a profiler run for each api, implemented framebuffers in opengl, seperated texture asset from texture graphics object

--- arbrook/core/src/core/assets/assetcache.cpp
+++ arbrook/core/src/core/assets/assetcache.cpp
@@ -3,9 +3,9 @@
 namespace rythe::core::assets
 {
 	template<typename AssetType>
-	inline std::unordered_map<rsl::id_type, std::unique_ptr<AssetImporter<AssetType>>> AssetCache<AssetType>::m_importers;
+	inline std::unordered_map<rsl::id_type, std::unique_ptr<AssetImporter<AssetType>>> AssetCache<AssetType>::m_importers{};
 	template<typename AssetType>
-	inline std::unordered_map<rsl::id_type, std::unique_ptr<AssetType>> AssetCache<AssetType>::m_assets;
+	inline std::unordered_map<rsl::id_type, std::unique_ptr<AssetType>> AssetCache<AssetType>::m_assets{};
 	template<typename AssetType>
-	inline std::unordered_map<rsl::id_type, std::string> AssetCache<AssetType>::m_names;
+	inline std::unordered_map<rsl::id_type, std::string> AssetCache<AssetType>::m_names{};
 }
