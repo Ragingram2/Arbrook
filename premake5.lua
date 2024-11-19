7954cb0b1f10b5d873f19eeec712db4d14922aaa
Rowan Ramsey (SharkBit) <blazinram@gmail.com>
2024-11-19 20:46:36 +0100

    updated a lot of stuff

--- premake5.lua
+++ premake5.lua
@@ -7,4 +7,8 @@ os.chdir(_MAIN_SCRIPT_DIR)
 
 local a = require("premake/rythe-build-system/rythe")
 
+-- proof of concept for global depenencies..... needs work
+-- local projects = dofile("premake/rythe-build-system/projects.lua")
+-- projects.loadProject("third_party/tracy", { name = "tracy", group = "third_party", location = "libraries/third_party/tracy", src = "premake5.lua" },"libraries/third_party/tracy", "tracy", "library")
+
 a.configure()
